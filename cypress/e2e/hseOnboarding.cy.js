8d1d47f8739b2fe5265a8358ac6af7d48789a7ae
Leeroy Steele <Leeroysteele@hotmail.com>
2024-10-31 15:59:44 +1300

    Final V2 -  Demo'd to team

--- cypress/e2e/hseOnboarding.cy.js
+++ cypress/e2e/hseOnboarding.cy.js
@@ -539,7 +539,7 @@ describe('Create new HSE company, induction process, users etc', () => {
 )
 
 
-  it.only('Tasklist added for remaining tasks on onboarding ticket', () => {
+  it('Tasklist added for remaining tasks on onboarding ticket', () => {
     // Do with webhook to Power Automate. Flow is here -> `https://make.powerautomate.com/environments/2e21e621-fcf3-eae1-a4d1-9e02b3152fc8/flows/96d7de0a-c2fb-4a1b-940e-e541d79058b4/runs/08584732557519775569424720085CU26`
     cy.addTasklistToTicket()
 
